b3c67fcb023f3a48eccb91ce38232587485e49e0
aureliano75 <aureliano75@live.com>
2021-12-03 13:34:43 -0800

    Update server.js
    Parse Imported CSV data to readable form

--- server.js
+++ server.js
@@ -5,20 +5,39 @@ const fs = require('fs');
 const parse = require('csv-parser')
 
 const salesData=[];
+let agents={}
 fs.createReadStream('./src/data/Sale-Data.csv')
     .pipe(parse())
     .on('data', function(csvrow) {
         salesData.push(csvrow);        
     })
     .on('end',function() {
-
+      agents = parseCsv(salesData)
+      console.log(agents)
     });
 
+function parseCsv(csvData) {
+  const data = {}
+  csvData.map((element) => {
+      if (data[element.agent] == null) {
+        data[element.agent] = {}
+        data[element.agent]["total"] = 0
+      }
+      if (data[element.agent] != null && data[element.agent][element['property-type']] == null) {
+        data[element.agent][element['property-type']] = 0
+      }
+      if (data[element.agent] != null && data[element.agent][element['property-type']] != null) {
+        data[element.agent][element['property-type']] += 1
+        data[element.agent]["total"] += 1
+      }
+  })
+  return data
+}
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
 
 // create a GET route
 app.get('/sales', (req, res) => {
-  res.send({ salesData: salesData }); 
+  res.send({ agents: agents }); 
 }); 
